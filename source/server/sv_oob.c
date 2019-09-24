84627c67c15d78553dd5931860984a4267d1d510
Warfork <51063252+Warfork@users.noreply.github.com>
2019-09-23 22:08:02 -0400

    Don't wait 5mins for first heartbeat
    https://github.com/mikejsavage/forksow/commit/afd2e6b07f6a2c646bed5dc5124f495ea76ea955
    
    I'm aware that "This is also made on purpose: admin often misconfigure their servers during the setup phase and take them online and offline at quick intervals. The server should not promote itself unless it's run for a few minutes without interruptions.".

--- source/server/sv_oob.c
+++ source/server/sv_oob.c
@@ -147,7 +147,8 @@ void SV_InitMaster( void )
 {
 	SV_ResolveMaster();
 
-	svc.nextHeartbeat = Sys_Milliseconds() + HEARTBEAT_SECONDS * 1000; // wait a while before sending first heartbeat
+		svc.nextHeartbeat = Sys_Milliseconds();
+
 }
 
 /*
