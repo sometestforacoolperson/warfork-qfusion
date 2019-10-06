7c4fc66d336086d6129668dacc8e1e7f48ccf36f
Warfork <51063252+Warfork@users.noreply.github.com>
2019-10-05 22:40:49 -0400

    combine RG numbers
    https://github.com/mikejsavage/forksow/commit/46acab26207e73b689b0ac011410af5bede4adb1
    https://github.com/mikejsavage/forksow/commit/2a0ae9a2ccec765acc48fe89ace76964305fd31e#diff-52c82d3be95e247646283ffac6c60421

--- source/game/g_weapon.cpp
+++ source/game/g_weapon.cpp
@@ -542,7 +542,8 @@ static void G_Fire_SunflowerPattern( edict_t *self, vec3_t start, vec3_t dir, in
 	float u;
 	float fi;
 	trace_t trace;
-
+ 
+ int hits[MAX_CLIENTS + 1] = { };
 	for( i = 0; i < count; i++ )
 	{
 		fi = i * 2.4; //magic value creating Fibonacci numbers
@@ -554,16 +555,24 @@ static void G_Fire_SunflowerPattern( edict_t *self, vec3_t start, vec3_t dir, in
 		{
 			if( game.edicts[trace.ent].takedamage )
 			{
-				G_Damage( &game.edicts[trace.ent], self, self, dir, dir, trace.endpos, damage, kick, stun, dflags, mod );
+				G_Damage( &game.edicts[trace.ent], self, self, dir, dir, trace.endpos, damage, kick, stun, dflags, MOD_RIOTGUN_W );
+                if( !GS_IsTeamDamage( &game.edicts[trace.ent].s, &self->s ) && trace.ent <= MAX_CLIENTS ) {
+				hits[trace.ent]++;
 			}
-			else
-			{
-				if( !( trace.surfFlags & SURF_NOIMPACT ) )
-				{
-				}
 			}
-		}
+ 
+ for( int i = 1; i <= MAX_CLIENTS; i++ ) {
+		if( hits[i] == 0 )
+			continue;
+		edict_t * target = &game.edicts[i];
+		edict_t * damage = G_SpawnEvent( EV_DAMAGE, 0, target->s.origin );
+		damage->r.svflags |= SVF_ONLYOWNER;
+		damage->s.ownerNum = ENTNUM( self );
+		damage->s.damage = HEALTH_TO_INT( hits[i] * damage );
 	}
+    
+		}   
+	} 
 }
 
 #if 0
