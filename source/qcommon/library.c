a23c9b818029c390663a16d20528a1a8b368d259
Leo <goleo@goleo.lan>
2019-12-31 21:26:38 +0200

    revert removal of pure check

--- source/qcommon/library.c
+++ source/qcommon/library.c
@@ -299,6 +299,15 @@ void *Com_LoadGameLibrary( const char *basename, const char *apifuncname, void *
 		return NULL;
 	}
 
+	// pure check
+	if( pure && !FS_IsPureFile( libname ) )
+	{
+		Com_Printf( "LoadLibrary (%s):(Unpure file)\n", libname );
+		Mem_TempFree( libname );
+		Mem_ZoneFree( gamelib );
+		return NULL;
+	}
+
 	temppath = Sys_Library_GetGameLibPath( libname, randomizer_time, randomizer );
 	tempname = ( char * )Mem_ZoneMalloc( strlen( temppath ) + 1 );
 	strcpy( tempname, temppath );
