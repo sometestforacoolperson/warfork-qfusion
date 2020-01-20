b01f5ec835fb6e4592588e8defc96d9e691b03b3
Leo <goleo@disroot.org>
2020-01-20 07:02:27 +0200

    remove unused variables

--- third-party/libRocket/Source/Core/Dictionary.cpp
+++ third-party/libRocket/Source/Core/Dictionary.cpp
@@ -479,10 +479,9 @@ void Dictionary::Clear()
 	DictionaryEntry *ep;
 	bool table_is_malloced;
 	int n_full;
-	int i, n;
+	int i;
 	
 	DICTIONARY_DEBUG_CODE( Log::Message(LC_CORE, Log::LT_ALWAYS, "Dictionary::Clear") );
-	n = mask + 1;
 	i = 0;
 	
 	table_is_malloced = table != small_table;
